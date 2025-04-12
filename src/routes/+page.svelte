613063fe1eb0ad48c6975a3279f535980d9d0388
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-12 21:24:17 +0900

    ちょっとやる気だそうと思ってる

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="p-10">
+    <h1 class="text-3xl font-bold">
+        AppName
+    </h1>    
+</div>
