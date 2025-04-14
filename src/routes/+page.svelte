5721764245fbcdd00336fe10b6de7638719896c4
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-15 00:01:03 +0900

    超絶適当にheader作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<div class="p-5">
-    <h1 class="text-3xl font-bold">
-        AppName
-    </h1>    
-</div>
+<script context="module" lang="ts">
+    
+</script>
+
+<main>
+    <div class="">
+        <header class=" bg-gray-200 p-3 backdrop-blur-2xl">
+            <h1 class="text-3xl font-light text-gray-900">
+                AppName
+            </h1>    
+        </header>
+    </div>
+</main>
