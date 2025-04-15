956c30d4b3a46e9d6091fefa55bc486982c885d0
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-15 23:10:38 +0900

    Todoリストの仮実装

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
-<script>
+<script lang="ts">
     import { Hamburger } from 'svelte-hamburgers';
 
     let open;
+
+    let todotitle = '';
+    let Todos: string[] =[]; 
+
+    function add(){
+        
+        if(todotitle === ''){
+            alert('タイトルを入力してください');
+            return;
+        }
+        alert(todotitle +`を追加しました`);
+        Todos = [...Todos, todotitle];
+        todotitle = '';
+    }
+
 </script>
 
 <main>
@@ -16,4 +31,22 @@
             </div>
         </header>
     </div>
+    <div class="flex justify-center">
+        <div>
+            <label>
+                <input class="bg-gray-200 rounded-md transition hover:bg-gray-300" bind:value={todotitle} />
+            </label>
+            <button class="p-1.5 bg-gray-200 rounded-md transition hover:bg-gray-400" on:click={add}>作成</button>
+        </div>
+    </div>
+    {#if Todos.length === 0}
+    <div class="flex justify-center">Todoを作成してください</div>
+    {:else}
+         <ul>
+              {#each Todos as todo}
+                  <li class=" bg-gray-200 rounded-md my-2">{todo}</li>
+              {/each}
+          </ul>
+    {/if}
+    
 </main>
