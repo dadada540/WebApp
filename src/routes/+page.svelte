d6148ebae65d8fcd6363b5ef5a42fd68f48bf6a8
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-15 16:30:09 +0900

    若干進んだ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
-<script context="module" lang="ts">
-    
+<script>
+    import { Hamburger } from 'svelte-hamburgers';
+
+    let open;
 </script>
 
 <main>
-    <div class="">
-        <header class=" bg-gray-200 p-3 backdrop-blur-2xl">
+    <div class="p-4">
+        <header class=" bg-gray-200 p-3 backdrop-blur-2xl rounded-2xl">
             <h1 class="text-3xl font-light text-gray-900">
                 AppName
-            </h1>    
+            </h1>  
+            <div class="fixed right-2 -mt-11">
+                <Hamburger />
+                
+            </div>
         </header>
     </div>
 </main>
