8c497ec64c4baa9d06a5b38495682f4454a22691
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-17 17:20:51 +0900

    修正したやつ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 <main>
 
     <Header/>
-    
+
     <div class="p-5">
         <div class="rounded-2xl border p-4">
             <div class="flex justify-center">
@@ -48,14 +48,6 @@
             {#if Todos.length === 0}
             <div class="flex justify-center font-bold">Todoを作成してください</div>
             {:else}
-<<<<<<< HEAD
-                <ul class="px-50">
-                    {#each Todos as todo}
-                        <li class="p-3 font-bold text-xl bg-gray-200 rounded-md my-2 pl-7">{todo}</li>
-                        <button class="bg-gray-200 ">削除</button>
-                    {/each}
-                </ul>
-=======
                  <ul class="px-50">
                       {#each Todos as todo}
                           <li class="p-3 font-bold text-xl bg-gray-200 rounded-md my-2 pl-7"><input type="checkbox">{todo}
@@ -63,7 +55,6 @@
                           </li>
                       {/each}
                   </ul>
->>>>>>> cdc24a5dfb4c00e8ad00320fc1b87f77cdc51fcb
             {/if}
         </div>
     </div>
