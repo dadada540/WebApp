648fffcf903393aae014ccd953397cfb923c27ba
DAI-KON <d2a7d2al7ps@gmail.com>
2025-04-17 15:51:56 +0900

    Headerの変更と削除の追加

---
+++ src/lib/header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Hamburger } from 'svelte-hamburgers';
+
+    let open;
+</script>
+
+<main>
+    <div class="p-4">
+        <header class=" bg-gray-200 p-3 backdrop-blur-2xl rounded-2xl">
+            <h1 class="text-3xl font-light text-gray-900">
+                使える機能はTodoリストだけです。
+            </h1>  
+            <div class="fixed right-2 -mt-11">
+                <Hamburger />
+                
+            </div>
+        </header>
+    </div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Hamburger } from 'svelte-hamburgers';
+    import Header from '$lib/header.svelte';
 
     let open;
 
@@ -12,44 +13,38 @@
             alert('タイトルを入力してください');
             return;
         }
+        else if(todotitle === `野獣先輩`){
+            alert(`不適切なワードは追加できません`)
+            return;
+        }
         alert(todotitle +`を追加しました`);
         Todos = [...Todos, todotitle];
         todotitle = '';
     }
 
 </script>
-
 <main>
-    <div class="p-4">
-        <header class=" bg-gray-200 p-3 backdrop-blur-2xl rounded-2xl">
-            <h1 class="text-3xl font-light text-gray-900">
-                使える機能はTodoリストだけです。
-            </h1>  
-            <div class="fixed right-2 -mt-11">
-                <Hamburger />
-                
+    <Header/>
+    <div class="p-5">
+        <div class="rounded-2xl border p-4">
+            <div class="flex justify-center">
+                <div>
+                    <label>
+                        <input class="bg-gray-200 rounded-md transition hover:bg-gray-300" bind:value={todotitle} />
+                    </label>
+                    <button class="p-1.5 bg-gray-200 rounded-md transition hover:bg-gray-400" on:click={add}>作成</button>
+                </div>
             </div>
-        </header>
-    </div>
-    <div class="rounded-md border p-4">
-        <div class="flex justify-center">
-            <div>
-                <label>
-                    <input class="bg-gray-200 rounded-md transition hover:bg-gray-300" bind:value={todotitle} />
-                </label>
-                <button class="p-1.5 bg-gray-200 rounded-md transition hover:bg-gray-400" on:click={add}>作成</button>
-            </div>
+            {#if Todos.length === 0}
+            <div class="flex justify-center font-bold">Todoを作成してください</div>
+            {:else}
+                 <ul class="px-50">
+                      {#each Todos as todo}
+                          <li class="p-3 font-bold text-xl bg-gray-200 rounded-md my-2 pl-7">{todo}</li>
+                          <button class="bg-gray-200 ">削除</button>
+                      {/each}
+                  </ul>
+            {/if}
         </div>
-        {#if Todos.length === 0}
-        <div class="flex justify-center font-bold">Todoを作成してください</div>
-        {:else}
-             <ul class="px-50">
-                  {#each Todos as todo}
-                      <li class="p-3 font-bold text-xl bg-gray-200 rounded-md my-2 pl-7">{todo}</li>
-                  {/each}
-              </ul>
-        {/if}
     </div>
-    
-    
 </main>
