e18fa790608f511e61a729dcbc1127698a54159b
abcdgfj <phamnamhd2308@gmail.com>
2025-06-12 18:27:55 +0900

    ちょっとresponsive

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -119,25 +119,27 @@
     <Header/>
 
     <div class="p-10">
-        <div class="rounded-2xl bg-gray-200 shadow-xl p-6">
-            <div class="flex justify-center">
-                <div>
+        <div class="rounded-2xl bg-gray-200 shadow-xl p-3">
+            <div class="flex  justify-center">
+                <div class="flex justify-center gap-3">
                     <label>
-                        <input class="bg-gray-200 rounded-md transition hover:bg-gray-300" bind:value={todotitle} />
+                        <input class="bg-gray-200 rounded-md transition hover:bg-gray-300 md:w-60 md:h-13 2xl:w-70 2xl:h-13" bind:value={todotitle} />
                     </label>
-                    <button class="border p-1.5 bg-gray-200 rounded-md transition hover:bg-blue-300" on:click={add}>作成</button>
+                    <button class="border p-1.5 bg-gray-200 rounded-md transition hover:bg-blue-300 md:text-2xl 2xl:text-3xl" on:click={add}>作成</button>
                 </div>
             </div>
             {#if Todos.length === 0}
             <div class="flex justify-center font-bold">Todoがまだ1つもありません</div>
             {:else}
-                <ul class="px-50">
+                <ul class="flex flex-col justify-center items-center">
                     {#each Todos as todo, index}
-                                <li class="p-3 font-bold text-xl border-1 border-solid rounded-md my-2 pl-7 flex felx-row  items-center shadow-md">
+                                <li class="p-3 font-bold text-xl border-1 border-solid rounded-md my-2 flex felx-row justify-center items-center shadow-md md:w-170 lg:w-170  2xl:h-30 2xl:w-300">
                                         <input type="checkbox" class="mr-4" checked={todo.completed} on:change={() => cheak(index)} />
-                                        <button class="bg-gray-500 border rounded-md p-1.5 w-15a mr-4 text-white transition hover:bg-red-800" on:click={() =>  del(index)}>削除</button>
-                                        <button class="bg-gray-500 border rounded-md p-1.5 w-15a mr-4 text-white transition hover:bg-red-800" on:click={() => openDatePicker(index)}>期限を設定</button>
-                                        {todo.title}
+                                        <button class="bg-gray-500 border rounded-md p-1.5 w-15a mr-4 text-white transition hover:bg-red-800 2xl:h-17 2xl:w-19" on:click={() =>  del(index)}>削除</button>
+                                        <button class="bg-gray-500 border rounded-md p-1.5 w-15a mr-4 text-white transition hover:bg-red-800 2xl:h-17" on:click={() => openDatePicker(index)}>期限を設定</button>
+                                        <div class="2xl:text-3xl 2xl:h-15 overflow-auto pl-5">
+                                            {todo.title}
+                                        </div>
                                         {#if selectedTodoIndex === index}
                                             <input
                                                 type="date"
