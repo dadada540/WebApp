1e5ee2f00855b72eb1ec260a47ce45554a1070dd
abcdgfj <168092244+abcdgfj@users.noreply.github.com>
2025-07-04 16:42:15 +0900

    ハンバーガーメニューの編集

--- src/lib/hamburgerMenu.svelte
+++ src/lib/hamburgerMenu.svelte
@@ -10,15 +10,21 @@
 </button>
 
 
-<div class="fixed right-0 w-full bg-gray-200 transition-transform duration-300"  style="transform: translateX({Open ? '0' : '100%'})">
-    <div class="bg-gray-500 p-6 rounded-2xl absolute right-0 w-55 ">
-        <div class="top-3">
-            <ul class="text-left flex flex-col gap-6  divide-y divide-white">
-                <li><a class="text-3xl" href="#">namu</a></li>
-                <li><a class="text-3xl" href="#">namu</a></li>
-                <li><a class="text-3xl" href="#">namu</a></li>
-                <li><a class="text-3xl" href="#">namu</a></li>
-            </ul>
+<div class="overflow-x-hidden">
+    <div class="fixed left-0 top-0 w-full bg-gray-200 z-30"  style="transform: translateX({Open ? '0' : '-120%'})">
+        <div class="bg-gray-500 p-6 rounded-2xl absolute right-0 w-55 ">
+            <button class="absolute top-2 right-4" on:click={() => Open = false}>
+                <XMark class="w-8 h-8 text-white" />
+            </button>
+
+            <div class="top-3">
+                <ul class="text-left flex flex-col gap-6  divide-y divide-white">
+                    <li><a class="text-3xl" href="#">namu</a></li>
+                    <li><a class="text-3xl" href="#">namu</a></li>
+                    <li><a class="text-3xl" href="#">namu</a></li>
+                    <li><a class="text-3xl" href="#">namu</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
